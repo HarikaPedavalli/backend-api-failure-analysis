a3a129ccf7f211ca95707043a8a25777e257eede
Harika Pedavalli <harikapedavalli@gmail.com>
2026-03-31 15:23:11 -0500

    Set up Flask app and register report routes

--- app/app.py
+++ app/app.py
@@ -1,4 +1,8 @@
-from views import get_reports
+from flask import Flask
+from routes import report_routes
+
+app = Flask(__name__)
+app.register_blueprint(report_routes, url_prefix="/api")
 
 if __name__ == "__main__":
-    print(get_reports())
+    app.run(debug=True)
